ec3082634d064bd4a6d394916982f14b119c1016
Gris Ge <fge@redhat.com>
2023-06-03 17:01:07 +0800

    Init
    Signed-off-by: Gris Ge <fge@redhat.com>

---
+++ wrong_async_sleep.rs
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: Apache-2.0
+
+// use tokio::time::sleep;
+use std::thread::sleep;
+use std::time::Duration;
+
+use futures::future::join;
+
+async fn foo() {
+    // sleep(Duration::from_secs(3)).await;
+    sleep(Duration::from_secs(3));
+    println!("foo() slept 3");
+}
+
+async fn bar() {
+    // sleep(Duration::from_secs(1)).await;
+    sleep(Duration::from_secs(1));
+    println!("bar() slept 1");
+}
+
+#[tokio::main()]
+async fn main() {
+    join(foo(), bar()).await;
+}
